e01a0f4d8d94c54edf2f72e489fcd02a0349a411
Marivaldo Sena <marivaldo.sena@live.com>
2017-12-19 21:48:32 -0200

    Add New and Create action Message Views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,28 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to messages_path, notice: 'Message was created successfully!' }
+        format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        format.json { render json: @message.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    def message_params
+      params.require(:message).permit(:title, :description)
+    end
 end
